38484119e4db87efe6db96c1163789b765a35c0b
Dan Klotz <danferth@gmail.com>
2018-08-28 17:06:38 +0000

    padding on inputs and a focus toggle for animation classes

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
     var honeypot = $('#your-email247htj');
     honeypot.hide();
 
+    $('input[type="text"], input[type="email"]').on('focus', function(e){
+      $(this).removeClass('fadeIn');
+      $(this).addClass('rubberBand');
+    });
     
     
 });
